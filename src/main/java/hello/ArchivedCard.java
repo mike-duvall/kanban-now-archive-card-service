2147278fcb018d3c536163d773cc8928b93f6e4f
Mike DuVall <mike.duvall@blackbaud.com>
2015-04-02 08:35:23 -0500

    Added archivedCard endpoint.  Updated dockerfile

---
+++ src/main/java/hello/ArchivedCard.java
@@ -0,0 +1,27 @@
+package hello;
+
+public class ArchivedCard {
+
+    private String text;
+    private String date;
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+}
---
+++ src/main/java/hello/ArchivedCardController.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ArchivedCardController {
+    
+    @RequestMapping("/archivedCard")
+    public List<ArchivedCard> archivedCard() {
+        List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
+        ArchivedCard card1 = new ArchivedCard();
+        card1.setText("Card1");
+        card1.setDate("04/02/2105");
+        archivedCardList.add(card1);
+
+        ArchivedCard card2 = new ArchivedCard();
+        card2.setText("Card2");
+        card2.setDate("04/02/2105");
+        archivedCardList.add(card2);
+
+        return archivedCardList;
+    }
+    
+}
