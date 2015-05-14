3d1fa21e9b066436ba0c5670d7365021bd60a2bc
Mike DuVall <mike.duvall@blackbaud.com>
2015-05-14 10:51:28 -0500

    Updated to do deploy to AWS.

--- src/main/java/hello/ArchivedCardController.java
+++ src/main/java/hello/ArchivedCardController.java
@@ -13,12 +13,12 @@ public class ArchivedCardController {
     public List<ArchivedCard> archivedCard() {
         List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
         ArchivedCard card1 = new ArchivedCard();
-        card1.setText("Card1-New1");
+        card1.setText("Card1-New2");
         card1.setDate("04/02/2105");
         archivedCardList.add(card1);
 
         ArchivedCard card2 = new ArchivedCard();
-        card2.setText("Card2-New1");
+        card2.setText("Card2-New2");
         card2.setDate("04/02/2105");
         archivedCardList.add(card2);
 
