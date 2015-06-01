581c185d2223a1746c711ba45fedc9c162984272
Mike DuVall <mike.duvall@blackbaud.com>
2015-06-01 07:11:26 -0500

    Updated hard coded results to use current day

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -14,11 +14,11 @@ public class Application {
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
     }
 
 }
--- src/main/java/hello/ArchivedCardController.java
+++ src/main/java/hello/ArchivedCardController.java
@@ -3,26 +3,39 @@ package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class ArchivedCardController {
 
+
     @RequestMapping("/archivedCards")
     public List<ArchivedCard> archivedCard() {
         List<ArchivedCard> archivedCardList = new ArrayList<ArchivedCard>();
         ArchivedCard card1 = new ArchivedCard();
+
+        String todayDateAsString = getTodayDateAsString();
+
         card1.setText("Take Claritin");
-        card1.setDate("6/1/2015");
+        card1.setDate(todayDateAsString);
         archivedCardList.add(card1);
 
         ArchivedCard card2 = new ArchivedCard();
         card2.setText("Save the whales");
-        card2.setDate("6/1/2015");
+        card2.setDate(todayDateAsString);
         archivedCardList.add(card2);
 
         return archivedCardList;
     }
-    
+
+    public String getTodayDateAsString() {
+        DateFormat df = new SimpleDateFormat("M/d/yyyy");
+        Date today = Calendar.getInstance().getTime();
+        return df.format(today);
+    }
 }
