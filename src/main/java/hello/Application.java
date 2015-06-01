f1ca235796688211c60cea4c6e9573c57951c4ca
Mike DuVall <mike.duvall@blackbaud.com>
2015-06-01 08:33:57 -0500

    Dummy change to test acceptance test problem

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+//        System.out.println("Let's inspect the beans provided by Spring Boot:");
         
 //        String[] beanNames = ctx.getBeanDefinitionNames();
 //        Arrays.sort(beanNames);
